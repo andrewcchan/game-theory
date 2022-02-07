3c3e8d37fa82de002514df98096807af1bcccc4d
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2022-02-06 22:26:59 -0800

    feat: test zero sum game

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import nashpy as nash
+
 
 """
 # Applied Game Theory Final Project
@@ -22,3 +24,8 @@ Andrew Chan
 """
 hello = 42
 st.write(f'Selected option: `{hello}`')
+
+A = [[1, -1], [-1, 1]]
+B = [[-1, 1], [1, -1]]
+matching_pennies = nash.Game(A, B)
+matching_pennies
